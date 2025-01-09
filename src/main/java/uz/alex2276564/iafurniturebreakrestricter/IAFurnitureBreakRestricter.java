08305d16b84f1572e3edc4668708c3b433bfa335
alex2276564 <you@example.com>
2025-01-09 07:13:12 +0300

    Add UpdateChecker

--- src/main/java/uz/alex2276564/iafurniturebreakrestricter/IAFurnitureBreakRestricter.java
+++ src/main/java/uz/alex2276564/iafurniturebreakrestricter/IAFurnitureBreakRestricter.java
@@ -3,15 +3,22 @@ package uz.alex2276564.iafurniturebreakrestricter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import uz.alex2276564.iafurniturebreakrestricter.listeners.IAFurnitureBreakListener;
+import uz.alex2276564.iafurniturebreakrestricter.utils.UpdateChecker;
 
 public final class IAFurnitureBreakRestricter extends JavaPlugin {
 
     @Override
     public void onEnable() {
         registerListeners();
+        checkUpdates();
     }
 
     private void registerListeners() {
         Bukkit.getPluginManager().registerEvents(new IAFurnitureBreakListener(), this);
+    }
+
+    private void checkUpdates() {
+        UpdateChecker updateChecker = new UpdateChecker(this, "alex2276564/IAFurnitureBreakRestricter");
+        updateChecker.checkForUpdates();
     }
 }
