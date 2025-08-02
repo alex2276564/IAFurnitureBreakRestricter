461d3081f619ee4654e1f17fbb62384bfd6f15d6
alex2276564 <you@example.com>
2025-08-02 13:54:08 +0300

    refactor

--- src/main/java/uz/alex2276564/iafurniturebreakrestricter/listeners/IAFurnitureBreakListener.java
+++ src/main/java/uz/alex2276564/iafurniturebreakrestricter/listeners/IAFurnitureBreakListener.java
@@ -5,7 +5,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import uz.alex2276564.iafurniturebreakrestricter.events.PlayerIAFurnitureBreakEvent;
 
 public class IAFurnitureBreakListener implements Listener {
