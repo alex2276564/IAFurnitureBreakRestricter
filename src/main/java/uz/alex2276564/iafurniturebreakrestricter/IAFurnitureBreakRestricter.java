253c1480a1decba1e57d0a76f3d22a9ae7210f7c
alex2276564 <email@example.com>
2026-06-20 22:37:45 +0500

    refactoring

--- src/main/java/uz/alex2276564/iafurniturebreakrestricter/IAFurnitureBreakRestricter.java
+++ src/main/java/uz/alex2276564/iafurniturebreakrestricter/IAFurnitureBreakRestricter.java
@@ -4,26 +4,32 @@ import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import uz.alex2276564.iafurniturebreakrestricter.listeners.IAFurnitureBreakListener;
+import uz.alex2276564.iafurniturebreakrestricter.utils.HttpUtils;
+import uz.alex2276564.iafurniturebreakrestricter.utils.UpdateChecker;
 import uz.alex2276564.iafurniturebreakrestricter.utils.runner.FoliaRunner;
 import uz.alex2276564.iafurniturebreakrestricter.utils.runner.Runner;
-import uz.alex2276564.iafurniturebreakrestricter.utils.UpdateChecker;
+
+import java.util.logging.Level;
 
 public final class IAFurnitureBreakRestricter extends JavaPlugin {
 
     @Getter
     private Runner runner;
 
+    @Getter
+    private HttpUtils httpUtils;
+
     @Override
     public void onEnable() {
         try {
             setupRunner();
+            setupHttpClient();
             registerListeners();
             checkUpdates();
 
             getLogger().info("IAFurnitureBreakRestricter has been enabled successfully!");
         } catch (Exception e) {
-            getLogger().severe("Failed to enable IAFurnitureBreakRestricter: " + e.getMessage());
-            e.printStackTrace();
+            getLogger().log(Level.SEVERE, "Failed to enable IAFurnitureBreakRestricter", e);
             getServer().getPluginManager().disablePlugin(this);
         }
     }
@@ -37,12 +43,16 @@ public final class IAFurnitureBreakRestricter extends JavaPlugin {
         }
     }
 
+    private void setupHttpClient() {
+        this.httpUtils = new HttpUtils();
+    }
+
     private void registerListeners() {
         Bukkit.getPluginManager().registerEvents(new IAFurnitureBreakListener(), this);
     }
 
     private void checkUpdates() {
-        UpdateChecker updateChecker = new UpdateChecker(this, "alex2276564/IAFurnitureBreakRestricter", runner);
+        UpdateChecker updateChecker = new UpdateChecker(this, "alex2276564/IAFurnitureBreakRestricter", runner, httpUtils);
         updateChecker.checkForUpdates();
     }
 
